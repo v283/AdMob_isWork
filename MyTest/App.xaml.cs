8326b5d8854992b8aa5a1834bb58bdab26aaa5d7
v283 <gandribidav@gmail.com>
2023-07-16 22:07:41 +0300

    Add project files.

---
+++ MyTest/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Plugin.MauiMTAdmob;
+
+namespace MyTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+
+	}
+}
---
+++ MyTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
